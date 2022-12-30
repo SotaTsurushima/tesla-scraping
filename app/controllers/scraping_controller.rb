10cc1d3d2f2b307be8abe53a14e47ae68d31c44d
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2022-12-30 19:14:03 +0900

    display title subtitle

--- app/controllers/scraping_controller.rb
+++ app/controllers/scraping_controller.rb
@@ -1,4 +1,39 @@
 class ScrapingController < ApplicationController
   def index
+    agent = Mechanize.new
+    # 取得したいページの取得
+    page = agent.get("https://ev-database.org/")
+    
+    # image
+    # image_elements = page.search('h2 a')
+    # @images = []
+    # image_elements.each do |ele|
+    #   @images.push(ele.
+    # end
+    
+    # title
+    title_elements = page.search('h2 a')
+    @titles = []
+    title_elements.each do |ele|
+      @titles.push(ele.inner_text)
+    end
+    
+    # subtitle
+    subtitle_elements = page.search('div .subtitle')
+    @subtitles = []
+    subtitle_elements.each do |ele|
+      @subtitles.push(ele.inner_text)
+    end
+    
+    # towweight
+    towweight_elements = page.search('.towweight')
+    @towweights = []
+    towweight_elements.each do |ele|
+      @towweights.push(ele.inner_text)
+    end
+
+    
+
+    
   end
 end
