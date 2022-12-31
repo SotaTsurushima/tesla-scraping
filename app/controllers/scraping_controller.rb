cc09e705f2fb7c21994ad4443084058822b8fb29
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2023-01-01 01:25:25 +0900

    implemented Germany, nethreland, UK price

--- app/controllers/scraping_controller.rb
+++ app/controllers/scraping_controller.rb
@@ -24,8 +24,48 @@ class ScrapingController < ApplicationController
     end
 
     # size
-    size_elements = page.search('.size-d')
+    # 取れてないやつあるっぽいので保留
+    size_elements = page.search('.size-') ||
     size_elements.each_with_index do |ele, i|
+      @tesla_data[i].push(ele.inner_text)
+    end
+
+    
+    binding.pry
+    
+
+    # number_of_seats
+    # number_of_seats_elements = page.search('.icons span')
+    # # title=Number of seatsで絞り込めるようにする
+    # binding.pry
+    # number_of_seats_elements.each_with_index do |ele, i|
+    #   @tesla_data[i].push(ele.inner_text)
+    # end
+
+    # tags_elements = page.search('.left span')
+    # # 加速時間
+    # tags_elements.each_with_index do |ele, i|
+    #   if ele.inner_text.include?("0 - 100") 
+    #     @tesla_data[i].push(ele.inner_text)
+      
+    #     binding.pry
+        
+    #   end
+    # end
+
+    # Germany price
+    germany_price_elements = page.search('.country_de')
+    germany_price_elements.each_with_index do |ele, i|
+      @tesla_data[i].push(ele.inner_text)
+    end
+    # Netherland price
+    netherland_price_elements = page.search('.country_nl')
+    netherland_price_elements.each_with_index do |ele, i|
+      @tesla_data[i].push(ele.inner_text)
+    end
+    # UK price
+    uk_price_elements = page.search('.country_nl')
+    uk_price_elements.each_with_index do |ele, i|
       @tesla_data[i].push(ele.inner_text)
     end
 
