fc7aef0137ce79267a6e69af0b1cf51e05b89bae
T$uru$hima <67181317+SotaTsurushima@users.noreply.github.com>
2023-01-02 01:25:29 +0900

    implemented fatcharge and changed method for longtht\y writing

--- app/controllers/scraping_controller.rb
+++ app/controllers/scraping_controller.rb
@@ -13,22 +13,17 @@ class ScrapingController < ApplicationController
   
     # subtitle
     subtitle_elements = page.search('div .subtitle')
-    subtitle_elements.each_with_index do |ele, i|
-      @tesla_data[i].push(ele.inner_text)
-    end
+    push_text_with_index(subtitle_elements)
     
     # towweight
     towweight_elements = page.search('.towweight')
-    towweight_elements.each_with_index do |ele, i|
-      @tesla_data[i].push(ele.inner_text)
-    end
+    push_text_with_index(towweight_elements)
+
 
     # size
-    # 取れてないやつあるっぽいので保留
     size_elements = page.search("span[@class*='size-']") 
-    size_elements.each_with_index do |ele, i|
-      @tesla_data[i].push(ele.inner_text)
-    end
+    push_text_with_index(size_elements)
+
 
     # number_of_seats
     # number_of_seats_elements = page.search("span[title*='Number of seats']")
@@ -48,6 +43,7 @@ class ScrapingController < ApplicationController
     top_speed_elements = page.search('.left .topspeed')
     driving_distance_elements = page.search('.left .erange_real')
     efficiency_elements = page.search('.left .efficiency')
+    fastcharge_elements = page.search('.left .fastcharge_speed_print')
     
     # 加速時間
     push_left_text(tags_elements, "0 - 100")
@@ -61,30 +57,27 @@ class ScrapingController < ApplicationController
     # 燃費？
     push_left_text(tags_elements, "Efficiency")
     push_left_text(efficiency_elements, "Wh/km")
-
+    # 充電速度
+    push_left_text(tags_elements, "Fastcharge")
+    push_left_text(fastcharge_elements, "km/h")
 
 
     # Germany price
     germany_price_elements = page.search('.country_de')
-    germany_price_elements.each_with_index do |ele, i|
-      @tesla_data[i].push(ele.inner_text)
-    end
+    push_text_with_index(germany_price_elements)
+ 
     # Netherland price
     netherland_price_elements = page.search('.country_nl')
-    netherland_price_elements.each_with_index do |ele, i|
-      @tesla_data[i].push(ele.inner_text)
-    end
+    push_text_with_index(netherland_price_elements)
+
     # UK price
     uk_price_elements = page.search('.country_nl')
-    uk_price_elements.each_with_index do |ele, i|
-      @tesla_data[i].push(ele.inner_text)
-    end
+    push_text_with_index(uk_price_elements)
 
     @tesla_data = Kaminari.paginate_array(@tesla_data).page(params[:page]).per(50)
 
   end
 
-  # 第一引数: 取得の値
   def push_left_text(elements, text)
     count = 0
     
@@ -93,6 +86,12 @@ class ScrapingController < ApplicationController
         @tesla_data[count].push(ele.inner_text)
         count += 1
       end
+    end
+  end
+
+  def push_text_with_index(elements)
+    elements.each_with_index do |ele, i|
+      @tesla_data[i].push(ele.inner_text)
     end
   end
 end
